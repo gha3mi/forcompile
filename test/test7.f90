5396e824d8802ea2410d8924e20da256293f3cb7
Seyed Ali Ghasemi <info@gha3mi.com>
2023-09-25 16:20:35 +0200

    Update test7 and test8. (#1)

--- test/test7.f90
+++ test/test7.f90
@@ -5,9 +5,12 @@ program test_7
    type(compiler_explorer) :: ce
 
    call ce%set_source("&
-      int main() { return 0; }&
+      program hello;&
+      write(*,*) 'Hello World';&
+      end program hello&
       ")
-   call ce%set_compiler_id('g82')
+
+   call ce%set_compiler_id('gfortran132')
 
    call ce%options%set_userArguments('-O3')
    call ce%options%set_compilerOptions(skipAsm=.false., executorRequest=.false.)
@@ -23,14 +26,12 @@ program test_7
       libraryCode=.false.,&
       trim=.false.,&
       debugCalls=.false.)
-   call ce%options%set_tools(id='clangtidytrunk', args='-checks=*')
-   call ce%options%set_libraries(id='range-v3', version='trunk')
-   call ce%options%set_libraries(id='fmt', version='400')
 
-   call ce%set_lang('c++')
+   call ce%set_lang('fortran')
    call ce%set_allowStoreCodeDebug(.true.)
 
    call ce%compile()
+
    call ce%finalize()
 
 end program test_7
--- test/test8.f90
+++ test/test8.f90
@@ -1,16 +1,13 @@
-program test_7
+program test_8
 
    use forcompile, only: compiler_explorer
    implicit none
    type(compiler_explorer) :: ce
 
    call ce%set_source("&
-      program hello;&
-      write(*,*) 'Hello World';&
-      end program hello&
+      int main() { return 0; }&
       ")
-
-   call ce%set_compiler_id('gfortran132')
+   call ce%set_compiler_id('g82')
 
    call ce%options%set_userArguments('-O3')
    call ce%options%set_compilerOptions(skipAsm=.false., executorRequest=.false.)
@@ -26,13 +23,15 @@ program test_7
       libraryCode=.false.,&
       trim=.false.,&
       debugCalls=.false.)
+   call ce%options%set_tools(id='clangtidytrunk', args='-checks=*')
+   call ce%options%set_libraries(id='range-v3', version='trunk')
+   call ce%options%set_libraries(id='fmt', version='400')
 
-   call ce%set_lang('fortran')
+   call ce%set_lang('c++')
    call ce%set_allowStoreCodeDebug(.true.)
 
    call ce%compile()
-
    call ce%finalize()
 
-end program test_7
+end program test_8
 
